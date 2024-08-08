3ac82d0773a21f5fc34c2f5dbc8f3fe3e612b549
Saranya Muthu <Rajjr2002>
2024-08-08 21:11:27 +0530

    created social media app

---
+++ src/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop : "1rem"}}>
+        Hi, I am Muthuraj. Together lets make Education free for everyone in the world   
+      </p>
+    </main>
+  )
+}
+
+export default About
---
+++ src/Missing.js
@@ -0,0 +1,13 @@
+const Missing = () => {
+  return (
+    <main className="Missing">
+      <h2>Page not Found</h2>
+      <p>Well, That's Disappointing.</p>
+      <p>
+        Visit Our HomePage
+      </p>
+    </main>
+  )
+}
+
+export default Missing
